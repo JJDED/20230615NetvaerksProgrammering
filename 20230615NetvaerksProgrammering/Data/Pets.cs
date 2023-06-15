290c44e6434d26a24c33aa5e29c3b7309274b388
Tec <Tec@SKAB4-PC5>
2023-06-15 13:31:42 +0200

    Add project files.

---
+++ 20230615NetvaerksProgrammering/Data/Pets.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace _20230615NetvaerksProgrammering.Data;
+
+public class Pets
+{
+    public Pets()
+    {
+    }
+
+    public Pets(int id, string? name, string? species, string? breed, string? colour, int speed, double price)
+    {
+        Id = id;
+        Name = name;
+        Species = species;
+        Breed = breed;
+        Colour = colour;
+        Speed = speed;
+        Price = price;
+    }
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Species { get; set; }
+    public string? Breed   { get; set; }
+    public string? Colour { get; set; }
+    public int Speed { get; set; }
+    public double Price { get; set; }
+}
---
+++ 20230615NetvaerksProgrammering/Data/PetsService.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.SqlClient;
+
+namespace _20230615NetvaerksProgrammering.Data;
+
+public class PetsService
+{
+    //Connection string shouldnt be public accessable
+    private string conString = "Data Source=192.168.1.2;Initial Catalog=Pets;User ID=sa;Password=Passw0rd;";
+
+    public Pets ReadPets()
+    {
+        //int id, string? name, string? brand, int rAM, string? gPU, string? cPU, float price)
+
+        return new Pets(1, "John", "Cat", "Sphinx", "Pink", 15, 6000);
+    }
+
+    public List<Pets> ReadPets(string query)
+    {
+        List<Pets> list = new();
+        using (SqlConnection con = new(conString))
+        {
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Pets pets = new();
+                pets.Id = (int)reader[0];
+                pets.Name = (string)reader[1];
+                pets.Species = (string)reader[2];
+                pets.Breed = (string)reader[3];
+                pets.Colour = (string)reader[4];
+                pets.Speed = (int)reader[5];
+                pets.Price = (double)reader[6];
+                list.Add(pets);
+            }
+            con.Close();
+        }
+        return list;
+    }
+
+    public void CreatePet(Pets pet)
+    {
+        using (SqlConnection con = new(conString))
+        {
+            string query = $"INSERT INTO Pets ([Name], Species, Breed, Colour, Speed, Price) VALUES (@Name, @Species, @Breed, @Colour, @Speed, @Price)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            if (pet.Name == null) pet.Name = "Name Not Set";
+            cmd.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar).Value = pet.Name;
+            if (pet.Species == null) pet.Species = "Species Not Set";
+            cmd.Parameters.Add("@Species", System.Data.SqlDbType.NVarChar).Value = pet.Species;
+            if (pet.Breed == null) pet.Breed = "Breed Not Set";
+            cmd.Parameters.Add("@Breed", System.Data.SqlDbType.NVarChar).Value = pet.Breed;
+            if (pet.Colour == null) pet.Colour = "Colour Not Set";
+            cmd.Parameters.Add("@Colour", System.Data.SqlDbType.NVarChar).Value = pet.Colour;
+            cmd.Parameters.Add("@Speed", System.Data.SqlDbType.Int).Value = pet.Speed;
+            cmd.Parameters.Add("@Price", System.Data.SqlDbType.Float).Value = pet.Price;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
