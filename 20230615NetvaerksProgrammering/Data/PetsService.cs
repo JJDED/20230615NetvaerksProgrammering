a1ff0aa1c41f5c077d952de9ac1f8435664e893c
Jamie Jamahl de la Sencerie El-Dessouky <jael2@elev.tec.dk>
2023-06-16 10:30:53 +0200

    20230616

--- 20230615NetvaerksProgrammering/Data/PetsService.cs
+++ 20230615NetvaerksProgrammering/Data/PetsService.cs
@@ -65,9 +65,32 @@ public class PetsService
     {
         using (SqlConnection con = new(conString))
         {
-            string query = $"DELETE FROM Pets WHERE Id = @id";
+            string query = $"DELETE FROM Pets WHERE Id = {id}";
             SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+            //cmd.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+    public void UpdatePet(Pets pet)
+    {
+        using (SqlConnection con = new(conString))
+        {
+            //ToDo Set Set values in query
+            string query = $"UPDATE Pets SET [Name] = @Name, Species=@Species, Breed=@Breed, Colour=@Colour, Speed=@Speed, Price=@Price WHERE id = {pet.Id}";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = pet.Id;
+            if (pet.Name == null) pet.Name = "Name Not Set";
+            cmd.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar).Value = pet.Name;
+            if (pet.Species == null) pet.Species = "Species Not Set";
+            cmd.Parameters.Add("@Species", System.Data.SqlDbType.NVarChar).Value = pet.Species;
+            if (pet.Breed == null) pet.Breed = "Breed Not Set";
+            cmd.Parameters.Add("@Breed", System.Data.SqlDbType.NVarChar).Value = pet.Breed;
+            if (pet.Colour == null) pet.Colour = "Colour Not Set";
+            cmd.Parameters.Add("@Colour", System.Data.SqlDbType.NVarChar).Value = pet.Colour;
+            cmd.Parameters.Add("@Speed", System.Data.SqlDbType.Int).Value = pet.Speed;
+            cmd.Parameters.Add("@Price", System.Data.SqlDbType.Float).Value = pet.Price;
             con.Open();
             cmd.ExecuteNonQuery();
             con.Close();
